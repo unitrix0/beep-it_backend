bdab89722c49cdaec8f0475efd566fb8bb0147bb
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2020-03-25 13:32:55 +0100

    Einrichtung für Zugriff von Tablet

--- BeepBackend/Startup.cs
+++ BeepBackend/Startup.cs
@@ -114,11 +114,20 @@ namespace BeepBackend
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseDefaultFiles();
             app.UseStaticFiles();
-            app.UseMvc(routes =>
+
+            if (_environment.IsProduction())
+            {
+                app.UseMvc(routes =>
                 {
                     routes.MapSpaFallbackRoute(name: "spa-fallback",
                         defaults: new { controller = "Fallback", action = "Index" });
                 });
+            }
+            else
+            {
+                app.UseMvc();
+            }
+            
         }
 
         private void ConfigureBearerToken(JwtBearerOptions options, IServiceCollection services)
