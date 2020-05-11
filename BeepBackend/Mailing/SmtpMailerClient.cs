3a174e139ec28e4a9d72c20b5c834947ba249a38
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2020-05-11 16:43:25 +0200

    Login Credentials für SmtpMailerClient

--- BeepBackend/Mailing/SmtpMailerClient.cs
+++ BeepBackend/Mailing/SmtpMailerClient.cs
@@ -3,16 +3,24 @@ using System.ComponentModel;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace BeepBackend.Mailing
 {
     public class SmtpMailerClient : IMailerClient
     {
+        private IConfigurationSection _config;
+
+        public SmtpMailerClient(IConfiguration config)
+        {
+            _config = config.GetSection("SmtpMailSettings");
+        }
+
         public Task Send(string toAdr, string subject, string msgTxt)
         {
             var client = new SmtpClient("exch.hive.loc")
             {
-                Credentials = new NetworkCredential("", "", "")
+                Credentials = new NetworkCredential(_config["User"], _config["Password"], _config["Domain"])
             };
             client.SendCompleted += OnSendCompleted;
 
